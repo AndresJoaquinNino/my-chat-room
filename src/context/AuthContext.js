27957cc3cbec41742898922060370c64d183eb90
AndresJoaquinNino <andresninorodil@gmail.com>
2022-02-13 16:57:28 -0400

    Removing state loading in context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,7 +6,6 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const login = sessionStorage.getItem("chat-session")
-    const [loading, setLoading] = useState(true);
     useEffect(() => {
         const logState = onAuthStateChanged(auth, (user) => {
             if(user){
@@ -14,13 +13,12 @@ export const AuthProvider = ({ children }) => {
             }else{
                 sessionStorage.setItem("chat-session",false)
             }
-            setLoading(false)
         })
         return logState()
     },[])
 
     return (
-        <AuthContext.Provider value={{login,loading}}>
+        <AuthContext.Provider value={{login}}>
             { children }
         </AuthContext.Provider>
     );
