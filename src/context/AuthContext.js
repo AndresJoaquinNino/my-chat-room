26f3d9b1c0d204b24cdb723c41bbef6d32963039
AndresJoaquinNino <andresninorodil@gmail.com>
2022-02-13 16:51:40 -0400

    Using sessionStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,17 +5,19 @@ import { onAuthStateChanged } from 'firebase/auth';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [login, setLog] = useState(false);
+    const login = sessionStorage.getItem("chat-session")
     const [loading, setLoading] = useState(true);
-
     useEffect(() => {
         const logState = onAuthStateChanged(auth, (user) => {
-            console.log('user =', user, 'condition = ', user != null)
-            setLog(user != null)
+            if(user){
+                sessionStorage.setItem("chat-session",true)
+            }else{
+                sessionStorage.setItem("chat-session",false)
+            }
             setLoading(false)
         })
         return logState()
-    },[auth])
+    },[])
 
     return (
         <AuthContext.Provider value={{login,loading}}>
