b0489d190c59e036f159641df77093bb4957e0a6
AndresJoaquinNino <andresninorodil@gmail.com>
2022-02-13 09:13:33 -0400

    Manage login status

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { auth } from '../config/firebase'
+import { onAuthStateChanged } from 'firebase/auth';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [isLog, setLog] = useState(false);
+    const [login, setLog] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const logState = onAuthStateChanged(auth, (user) => {
+            console.log('user =', user, 'condition = ', user != null)
+            setLog(user != null)
+            setLoading(false)
+        })
+        return logState()
+    },[auth])
+
     return (
-        <AuthContext.Provider value={{isLog, setLog}}>
+        <AuthContext.Provider value={{login,loading}}>
             { children }
         </AuthContext.Provider>
     );
