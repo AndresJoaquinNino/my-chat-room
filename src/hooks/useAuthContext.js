aeeaf5f9398811417195e288d5a79ea172fbfd44
AndresJoaquinNino <andresninorodil@gmail.com>
2022-02-13 16:53:27 -0400

    Returning an Array

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -3,8 +3,9 @@ import { useContext } from "react"
 
 const useAuthContext = () => {
     const context = useContext(AuthContext);
+    const { login, loading } = context;
     if(!context) throw new Error('There is no AuthContext')
-    return context;
+    return [login, loading];
 }
 
 export default useAuthContext;
