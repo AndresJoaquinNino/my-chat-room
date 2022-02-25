a96c3399a3613f970d8057dd00b73adbeca89c53
AndresJoaquinNino <andresninorodil@gmail.com>
2022-02-25 11:14:31 -0400

    adding hour of message sended

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -7,13 +7,17 @@ import useAuthContext from '../hooks/useAuthContext';
 import SendIcon from '@mui/icons-material/Send';
 import LogoutIcon from '@mui/icons-material/Logout';
 import ScrollToBottom from '../components/ScrollToBottom'
+import moment from 'moment';
+
+const usersColor = {};
+
 const Chat = () => {
+
     const [isLog, logout] = useAuthContext();
     const customOrder = orderBy("createAt","asc");
     const customWhere = where("createAt","!=","");
     const customQuery = query(messagesCollection,customWhere,customOrder);
     const [messages, loading] = useCollection(customQuery)
-
     const [message, setMessage] = useState("");
 
     const handleChange = ({target}) => {
@@ -72,16 +76,36 @@ const Chat = () => {
 }
 
 const Message = ({messageData}) => {
-    const { message, photoURL, uid } = messageData;
+    const { message, photoURL, uid, createAt, displayName } = messageData;
+    const messageDate = createAt ? createAt.toDate() : new Date();
+    const messageTime = moment(messageDate).format('hh:mm A');
     const userId = auth.currentUser.uid;
     const userMessage = userId === uid;
+    usersColor[uid] = usersColor[uid] ? usersColor[uid] : getRandomColor();
+
     return(
         <li className={`message ${userMessage ? 'message-user':''}`}>
-            <img className='avatar' src={photoURL} />
-            <span className={`text-message ${userMessage ? 'text-message-user':''}`}>
-                {message}
-            </span>
+            <img className='avatar' src={photoURL || '../img/avatardefault.png'} alt=''/>
+            <p className={`text-message ${userMessage ? 'text-message-user':''}`} >
+                {
+                    !userMessage
+                    &&
+                    <span className='user-message' style={{'color': usersColor[uid]}}>{displayName}</span>
+                }
+                <span>{message}</span>
+                <span className={`label-message ${userMessage ? 'label-message-user':''}`}>{messageTime}</span>
+            </p>
         </li>
     )
 }
+
+const getRandomColor = () => {
+    const characters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += characters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 export default Chat;
